a6370a54a94dfb171d52fe88718022237b5eb82e
Alan-oliveir <alandeog@poli.ufrj.br>
2023-03-26 15:24:19 -0300

    modified:   main.py

--- main.py
+++ main.py
@@ -16,18 +16,35 @@ PATH = os.path.dirname(os.path.realpath(__file__))
 
 def selecionar_imagem():
 
-    global image_path
+    global image_path    
     
-    #Todo - teste para não aceitar outra extensão a não de imagem
-    image_path = fd.askopenfilename()
-    image = ctk.CTkImage(Image.open(image_path), size=(275,275))
-    FrameImage.l_image.configure(image=image)
+    image_path = fd.askopenfilename(filetypes=[('JPEG Files', '.jpg .jpeg'), ('PNG Files', '.png')])
 
-# Inserir
+    try:
+        image = ctk.CTkImage(Image.open(image_path), size=(275,275))
+        FrameImage.l_image.configure(image=image)    
+    
+    except:
+        messagebox.showerror('Erro', 'Não foi possível identificar a imagem. Por favor, confira o tipo de arquivo e selecione novamente.') 
+
+def delete_entries():
+        
+    FrameForm.e_nome.delete(0, 'end')
+    FrameForm.e_local.delete(0, 'end')
+    FrameForm.e_descricao.delete(0, 'end')
+    FrameForm.e_model.delete(0, 'end')
+    FrameForm.e_data_compra.configure(state="normal")
+    FrameForm.e_data_compra.delete(0, 'end')
+    FrameForm.e_data_compra.configure(state="disabled")   
+    FrameForm.e_valor.delete(0, 'end')
+    FrameForm.e_serial.delete(0, 'end')
+    FrameImage.l_image.configure(image=None)
+
 def inserir():
          
     global table, image_path
 
+    # Insert the data list with the form entries
     nome = FrameForm.e_nome.get()
     local = FrameForm.e_local.get()
     descricao = FrameForm.e_descricao.get()
@@ -55,20 +72,12 @@ def inserir():
         messagebox.showinfo('Sucesso', 'Os dados foram inseridos com sucesso')
 
     # Delete entries of FrameForm, table and image.
-    FrameForm.e_nome.delete(0, 'end')
-    FrameForm.e_local.delete(0, 'end')
-    FrameForm.e_descricao.delete(0, 'end')
-    FrameForm.e_model.delete(0, 'end')
-    FrameForm.e_data_compra.configure(state="normal")
-    FrameForm.e_data_compra.delete(0, 'end')
-    FrameForm.e_data_compra.configure(state="disabled")   
-    FrameForm.e_valor.delete(0, 'end')
-    FrameForm.e_serial.delete(0, 'end')
-    FrameImage.l_image.configure(image=None)
+    delete_entries()
 
     for item in table.get_children():
         table.delete(item)
 
+    # Update info of the table, qtd labels and image
     lista_itens = ver_form()
     for item in lista_itens:
         table.insert('', 'end', values=item)
@@ -80,7 +89,6 @@ def inserir():
     FrameText.textbox_value.configure(text="R$ {:,.2f}".format(sum(quantidade)))
     FrameText.textbox_qte_itens.configure(text=f"{len(quantidade)}")
 
-# funcao atualizar
 def atualizar():
     
     global table_view_list
@@ -89,18 +97,9 @@ def atualizar():
         table_view_data = table.focus()
         table_view_dic = table.item(table_view_data)
         table_view_list = table_view_dic['values']
-
-        # Delete previus values
-        FrameForm.e_nome.delete(0, 'end')
-        FrameForm.e_local.delete(0, 'end')
-        FrameForm.e_descricao.delete(0, 'end')
-        FrameForm.e_model.delete(0, 'end')   
-        FrameForm.e_data_compra.configure(state="normal")
-        FrameForm.e_data_compra.delete(0, 'end')
-        FrameForm.e_data_compra.configure(state="disabled") 
-        FrameForm.e_valor.delete(0, 'end')
-        FrameForm.e_serial.delete(0, 'end')
-        FrameImage.l_image.configure(image=None)    
+                
+        # Delete entries of FrameForm, table and image.
+        delete_entries() 
 
         # Get values and image of item selected
         FrameForm.e_nome.insert(0, table_view_list[1])
@@ -117,7 +116,7 @@ def atualizar():
         FrameImage.l_image.configure(image=image)   
 
         # Update state of buttons 
-        FrameButton.button_confirm.configure(state="normal")
+        FrameButton.button_update.configure(state="normal")
         FrameButton.button_delete.configure(state="normal")        
 
     except IndexError:
@@ -127,6 +126,7 @@ def update():
 
     global table_view_list, table
     
+    # Update the list with the form entries
     nome = FrameForm.e_nome.get()
     local = FrameForm.e_local.get()
     descricao = FrameForm.e_descricao.get()
@@ -136,10 +136,7 @@ def update():
     serie = FrameForm.e_serial.get()           
     imagem = table_view_list[8]
     id = int(table_view_list[0])
-
-    #if imagem == '':
-    #    imagem = FrameForm.e_serial.insert(0, table_view_list[7])
-
+     
     lista_atualizar = [nome, local, descricao, model, data, valor, serie, imagem, id]    
     for i in lista_atualizar:
         if i=='':
@@ -150,20 +147,12 @@ def update():
     messagebox.showinfo('Sucesso', 'Os dados foram atualizados com sucesso')
 
     # Delete entries of FrameForm, table and image.
-    FrameForm.e_nome.delete(0, 'end')
-    FrameForm.e_local.delete(0, 'end')
-    FrameForm.e_descricao.delete(0, 'end')
-    FrameForm.e_model.delete(0, 'end')
-    FrameForm.e_data_compra.configure(state="normal")
-    FrameForm.e_data_compra.delete(0, 'end')
-    FrameForm.e_data_compra.configure(state="disabled")   
-    FrameForm.e_valor.delete(0, 'end')
-    FrameForm.e_serial.delete(0, 'end')
-    FrameImage.l_image.configure(image=None)
+    delete_entries()
 
     for item in table.get_children():
         table.delete(item)
 
+    # Update info of the table, qtd labels and image
     lista_itens = ver_form()
     for item in lista_itens:
         table.insert('', 'end', values=item)
@@ -175,11 +164,10 @@ def update():
     FrameText.textbox_value.configure(text="R$ {:,.2f}".format(sum(quantidade)))
     FrameText.textbox_qte_itens.configure(text=f"{len(quantidade)}")
 
-    # Update state of button_confirm 
-    FrameButton.button_confirm.configure(state="disabled")
+    # Update state of button_update 
+    FrameButton.button_update.configure(state="disabled")
     FrameButton.button_delete.configure(state="disabled")
 
-# funcao deletar
 def deletar():
 
     global table_view_list, table
@@ -188,21 +176,13 @@ def deletar():
     deletar_form([id])
     messagebox.showinfo('Sucesso', 'Os dados foram deletados com sucesso')
 
-    # Delete entries of FrameForm, table and image.
-    FrameForm.e_nome.delete(0, 'end')
-    FrameForm.e_local.delete(0, 'end')
-    FrameForm.e_descricao.delete(0, 'end')
-    FrameForm.e_model.delete(0, 'end')
-    FrameForm.e_data_compra.configure(state="normal")
-    FrameForm.e_data_compra.delete(0, 'end')
-    FrameForm.e_data_compra.configure(state="disabled")   
-    FrameForm.e_valor.delete(0, 'end')
-    FrameForm.e_serial.delete(0, 'end')
-    FrameImage.l_image.configure(image=None)
+    # Delete entries of FrameForm, table and image
+    delete_entries()
 
     for item in table.get_children():
         table.delete(item)
 
+    # Update info of the table, qtd labels and image
     lista_itens = ver_form()
     for item in lista_itens:
         table.insert('', 'end', values=item)
@@ -216,18 +196,7 @@ def deletar():
 
     # Update state of buttons 
     FrameButton.button_delete.configure(state="disabled")
-    FrameButton.button_confirm.configure(state="disabled")
-
-'''# Função para abrir imagem
-def visualizar_imagem():
-
-    table_view_data = table.focus()
-    table_view_dic = table.item(table_view_data)
-    table_view_list = table_view_dic['values']
-
-    image_path=table_view_list[8]
-    image = ctk.CTkImage(Image.open(image_path), size=(275,275))
-    FrameImage.l_image.configure(image=image)'''
+    FrameButton.button_update.configure(state="disabled")
 
 
 # ==================== Frames =========================
@@ -235,14 +204,15 @@ def visualizar_imagem():
 class FrameTitleBar(ctk.CTkFrame):
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
-
+     
+        # Create 1x3 grid system -- column configure
         self.columnconfigure(2, weight=1)
         
-        # Font object:
+        # Font object
         font_title = ctk.CTkFont(family="Verdana", size=24, weight="bold")
         font_option_btn = ctk.CTkFont(size=13, weight="bold")  
 
-        # Open image:
+        # Open image
         app_img = ctk.CTkImage(Image.open(PATH + "/images/cadastre-se.png"), size=(50,50))
         option_img = ctk.CTkImage(Image.open(PATH + "/images/settings.png"), size=(25,25))
 
@@ -261,43 +231,51 @@ class FrameForm(ctk.CTkFrame):
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
         
+        # Create 8x2 grid system
         self.rowconfigure((0, 1, 2, 3, 4, 5, 6, 7), weight=1)
         self.columnconfigure((0, 1), weight=1)
 
         # Font object:
-        font_form = ctk.CTkFont(size=14, weight="bold")        
+        font_form = ctk.CTkFont(size=14, weight="normal")        
 
         # Add widgets onto the FrameForm
+        # Name
         l_nome = ctk.CTkLabel(self, text="Nome:",font=font_form, anchor=ctk.W)
         l_nome.grid(row=0, column=0, padx=10, pady=(10,5), stick="w")
         FrameForm.e_nome = ctk.CTkEntry(self, width=225)
         FrameForm.e_nome.grid(row=0, column=1, padx=10, pady=(10,5), stick="ne")
 
+        # Area
         l_local = ctk.CTkLabel(self, text="Área:", font=font_form, anchor=ctk.W)
         l_local.grid(row=1, column=0, padx=10, stick="w")
         FrameForm.e_local = ctk.CTkEntry(self, width=225)
         FrameForm.e_local.grid(row=1, column=1, padx=10, pady=5, stick="ne")
 
+        # Description
         l_descricao = ctk.CTkLabel(self, text="Descrição:",font=font_form, anchor=ctk.W)
         l_descricao.grid(row=2, column=0, padx=10, stick="w")
         FrameForm.e_descricao = ctk.CTkEntry(self, width=225)
         FrameForm.e_descricao.grid(row=2, column=1, padx=10, pady=5, stick="ne")
 
+        # Model
         l_model = ctk.CTkLabel(self, text="Marca ou modelo:", font=font_form, anchor=ctk.W)
         l_model.grid(row=3, column=0, padx=10, stick="w")
         FrameForm.e_model = ctk.CTkEntry(self, width=225)
         FrameForm.e_model.grid(row=3, column=1, padx=10, pady=5, stick="ne") 
 
+        # Price
         l_valor = ctk.CTkLabel(self, text="Valor da compra:", font=font_form, anchor=ctk.W)
         l_valor.grid(row=4, column=0, padx=10, stick="w")
         FrameForm.e_valor = ctk.CTkEntry(self, width=225)
         FrameForm.e_valor.grid(row=4, column=1, padx=10, pady=5, stick="ne")
 
+        # Serial number
         l_serial = ctk.CTkLabel(self, text="Número de série:", font=font_form, anchor=ctk.W)
         l_serial.grid(row=5, column=0, padx=10, stick="w")
         FrameForm.e_serial = ctk.CTkEntry(self, width=225)
         FrameForm.e_serial.grid(row=5, column=1, padx=10, pady=5, stick="se")
 
+        # Date
         l_cal = ctk.CTkLabel(self, text="Data da compra:", font=font_form, anchor=ctk.W)
         l_cal.grid(row=6, column=0, padx=(10,0), stick="we")
         FrameForm.e_data_compra = ctk.CTkEntry(self, width=85)
@@ -305,6 +283,7 @@ class FrameForm(ctk.CTkFrame):
         button_date = ctk.CTkButton(self, width=120, text="Selecionar Data", font=font_form, command=lambda: self.create_toplevel_date("compra"))
         button_date.grid(row=6, column=1, padx=10, pady=5, stick="e")
 
+        # Image
         l_view = ctk.CTkLabel(self, text="Imagem do item:", font=font_form, width=120, anchor=ctk.W)
         l_view.grid(row=7, column=0, padx=10, stick="w")
         button_view = ctk.CTkButton(self, width=224, text="Selecionar Imagem", font=font_form, command=selecionar_imagem)
@@ -315,6 +294,7 @@ class FrameForm(ctk.CTkFrame):
         
         global date_window, cal
 
+        # CTkToplevel window configuration
         date_window = ctk.CTkToplevel(self)
         date_window.geometry("325x365")
         date_window.title("Selecionar Data")
@@ -359,29 +339,25 @@ class FrameButton(ctk.CTkFrame):
         update_img = ctk.CTkImage(Image.open(PATH + "/images/refresh.png"), size=(40,40))
         confirm_img = ctk.CTkImage(Image.open(PATH + "/images/configuration.png"), size=(40,40))
         clear_img = ctk.CTkImage(Image.open(PATH + "/images/trash.png"), size=(40,40))
-        #view_img = ctk.CTkImage(Image.open(PATH + "/images/management.png"), size=(40,40))
-       
-        # Create 6x1 grid system -- row configuration
-        self.rowconfigure((0, 1, 2, 3, 4), weight=1)
+               
+        # Create 4x1 grid system -- row configuration
+        self.rowconfigure((0, 1, 2, 3), weight=1)
                 
         # Font object
         font_button = ctk.CTkFont(size=13, weight="bold")
 
         # Add widgets onto the FrameButton
-        button_add = ctk.CTkButton(self, width=175, text="Adicionar", image=add_img, compound="left", font=font_button, command=inserir)        
-        button_add.grid(row=0, column=0, padx=5, pady=5)
+        FrameButton.button_add = ctk.CTkButton(self, width=175, text="Adicionar", image=add_img, compound="left", font=font_button, command=inserir)        
+        FrameButton.button_add.grid(row=0, column=0, padx=5, pady=5)
         
-        button_update = ctk.CTkButton(self, width=175, text="Atualizar", image=update_img, compound="left", font=font_button, command=atualizar)
-        button_update.grid(row=1, column=0, padx=5, pady=5)
+        FrameButton.button_view = ctk.CTkButton(self, width=175, text="Visualizar", image=update_img, compound="left", font=font_button, command=atualizar)
+        FrameButton.button_view.grid(row=1, column=0, padx=5, pady=5)
         
-        FrameButton.button_confirm = ctk.CTkButton(self, width=175, text="Confirmar", image=confirm_img, state="disabled", font=font_button, command=update)
-        FrameButton.button_confirm.grid(row=2, column=0, padx=5, pady=5)
+        FrameButton.button_update = ctk.CTkButton(self, width=175, text="Atualizar", image=confirm_img, state="disabled", font=font_button, command=update)
+        FrameButton.button_update.grid(row=2, column=0, padx=5, pady=5)
 
-        FrameButton.button_delete = ctk.CTkButton(self, width=175, text="Delete",image=clear_img, compound="left", state="disabled", font=font_button, command=deletar) 
+        FrameButton.button_delete = ctk.CTkButton(self, width=175, text="Excluir",image=clear_img, compound="left", state="disabled", font=font_button, command=deletar) 
         FrameButton.button_delete.grid(row=3, column=0, padx=5, pady=5)
-
-        #button_view = ctk.CTkButton(self, width=175, text="Ver Item", image=view_img, compound="left", font=font_button, command=visualizar_imagem)
-        #button_view.grid(row=4, column=0, padx=5, pady=5)
 
 
 class FrameText(ctk.CTkFrame):
@@ -412,11 +388,12 @@ class FrameTableInfo(ctk.CTkScrollableFrame):
 
         global table
 
+        # Create 2x3 grid system
         self.columnconfigure((0, 2), weight=1)
         self.columnconfigure(1, weight=30)
         
         # Font object
-        font_label = ctk.CTkFont(size=14, weight="bold")
+        font_label = ctk.CTkFont(size=13, weight="bold")
 
         # Open image
         search_img = ctk.CTkImage(Image.open(PATH + "/images/magnifying-glass.png"), size=(25,25))
@@ -446,12 +423,12 @@ class FrameTableInfo(ctk.CTkScrollableFrame):
             table.column(col, width=width_col[i])
             i += 1
 
+        # Update info of the table, qtd labels and image
         lista_itens = ver_form()
         for item in lista_itens:
             table.insert('', 'end', values=item)
 
         quantidade = []
-
         for item in lista_itens:
             quantidade.append(item[6])
 
@@ -465,20 +442,20 @@ class FrameImage(ctk.CTkFrame):
 
         # Font object
         font_img_label = ctk.CTkFont(size=14, weight="bold")
-
+        
+        # Add widgets onto the FrameImage
         FrameImage.l_image_title = ctk.CTkLabel(self, text="Foto do Item", width=300, font=font_img_label)
         FrameImage.l_image_title.grid(row = 0, column = 0, padx = 15, pady = 2, stick = "new")
        
         FrameImage.l_image = ctk.CTkLabel(self, text="")
         FrameImage.l_image.grid(row = 1, column = 0, padx = 15, pady = (0,15), stick = "nsew")
 
-        #todo
-        # quando selecionar um item da treeview mostra a foto
 
 class App(ctk.CTk): 
     def __init__(self):
         super().__init__()  
-              
+
+        # Configure window   
         self.resizable(False, False)
         self.title("")
 
@@ -488,10 +465,10 @@ class App(ctk.CTk):
 
         # Add frames to App        
         self.FrameTitleBar = FrameTitleBar(master=self)
-        self.FrameTitleBar.grid(row=0, column=0, columnspan=4, stick="new")
+        self.FrameTitleBar.grid(row=0, column=0, pady=(3, 1), padx=3, columnspan=4, stick="new")
 
         self.FrameForm = FrameForm(master=self)
-        self.FrameForm.grid(row=1, column=0, pady=2, padx=1, stick="nsw")
+        self.FrameForm.grid(row=1, column=0, pady=2, padx=(3, 1), stick="nsw")
 
         self.FrameButton = FrameButton(master=self)
         self.FrameButton.grid(row=1, column=1, pady=2, padx=1, stick="nsw")
@@ -500,10 +477,10 @@ class App(ctk.CTk):
         self.FrameText.grid(row=1, column=2, pady=2, padx=1, stick="nsw")
 
         self.FrameImage = FrameImage(master=self)
-        self.FrameImage.grid(row=1, column=3, pady=2, padx=2, stick="nsw")
+        self.FrameImage.grid(row=1, column=3, pady=2, padx=(2, 3), stick="nsw")
 
         self.FrameTableInfo = FrameTableInfo(master=self)
-        self.FrameTableInfo.grid(row=2, column=0, columnspan=4, pady=2, padx=1, stick="sew")
+        self.FrameTableInfo.grid(row=2, column=0, columnspan=4, pady=(2, 5), padx=3, stick="sew")
 
 app = App()
 app.mainloop()
